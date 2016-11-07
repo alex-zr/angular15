5a58ffd276bcd0733f9d25345eaa6a085c3eb152
alex-zr <1a9l7e#X>
2016-11-07 14:28:43 +0200

    components routing

---
+++ comp_route/components/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dev on 11/7/16.
+ */
+'use strict';
+angular.module('app', ['ui.router'])
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('foo', {
+        resolve: { mydata: (Service) => Service.getData() },
+        component: 'helloWorld'
+      })
+      .state({  name: 'home',  url: '/home',  component: 'home' })
+    })
+  .component("helloWorld",{
+      templateUrl: "templates/helloWorld.html",
+      bindings: { name: '@' }
+  })
+  .component("home",{
+    templateUrl: "templates/home.html",
+    bindings: { name: '@' }
+  });
+
